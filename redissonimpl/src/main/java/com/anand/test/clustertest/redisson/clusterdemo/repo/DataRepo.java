140da3926b7f8b59b2f32cf0ef4847326f8958d8
Anand Bose <anandbose@anands-mbp.attlocal.net>
2017-11-08 10:13:30 -0800

    Added new peojects

---
+++ redissonimpl/src/main/java/com/anand/test/clustertest/redisson/clusterdemo/repo/DataRepo.java
@@ -0,0 +1,42 @@
+package com.anand.test.clustertest.redisson.clusterdemo.repo;
+
+import org.redisson.api.RBucket;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Repository;
+
+import com.anand.test.clustertest.redisson.clusterdemo.aop.RCacheEvict;
+import com.anand.test.clustertest.redisson.clusterdemo.aop.RCacheGet;
+import com.anand.test.clustertest.redisson.clusterdemo.aop.RCachePut;
+import com.anand.test.clustertest.redisson.clusterdemo.models.ExampleData;
+
+@Repository
+public class DataRepo {
+
+	
+	@RCachePut(cacheName = "test", key = "#data.cachekey" , ttl="#redis.ttl.test")
+	public ExampleData cacheData(ExampleData data)
+	{
+
+		return data;
+	}
+	
+	@RCacheGet(cacheName = "test",key = "key")
+	public ExampleData fetchData(String key)
+	{
+
+		return null;
+
+	}
+	
+	@RCacheEvict(cacheName = "test" , scope = "KEY" , key ="testkey")
+	public void delKey(String testkey)
+	{
+	}
+	@RCacheEvict(cacheName = "test" , scope = "ALL" )
+	public void delAllKeys()
+	{
+	}	
+	
+}
