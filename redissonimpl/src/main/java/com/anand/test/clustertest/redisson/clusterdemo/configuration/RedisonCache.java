19883a09115393353d2bf1c3a056a435dabb2a5c
anandbose <anandbose@anands-mbp>
2017-11-14 09:32:14 -0800

    Changed to config

--- redissonimpl/src/main/java/com/anand/test/clustertest/redisson/clusterdemo/configuration/RedisonCache.java
+++ redissonimpl/src/main/java/com/anand/test/clustertest/redisson/clusterdemo/configuration/RedisonCache.java
@@ -5,13 +5,16 @@ import java.io.File;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RedisonCache {
+  @Value(value = "${redis.config}")
+  private String filename="";
  public @Bean RedissonClient createRedissonBean() throws Exception
  {
-	 Config config = Config.fromYAML(new File("/Users/anandbose/tmob/Redis/redis-local-cluster.yml"));
+	 Config config = Config.fromYAML(new File(filename));
 	 RedissonClient client = Redisson.create(config);
 	 return client;
  }
